e0f1fe0023f87d2dca402209de54f401a88960c1
MrJorgx <jhidaf00@estudiantes.unileon.es>
2025-12-01 18:55:41 +0100

    Cambios actualizados

--- Ejercicios/Ejercicios.Frontend/Program.cs
+++ Ejercicios/Ejercicios.Frontend/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
 using Ejercicios.Frontend;
+using Ejercicios.Frontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -8,6 +10,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5231/") });
+
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthenticationStateProvider>());
+builder.Services.AddAuthorizationCore();
 
 builder.Logging.SetMinimumLevel(LogLevel.Information);
 
